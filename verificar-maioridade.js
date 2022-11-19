180c43d8bad2c9c18591687ef2e302bb6b970ba7
Matheus Ebert <99367332+MatheusEbert010@users.noreply.github.com>
2022-11-19 02:25:17 -0300

    Adicionado via upload

---
+++ verificar-maioridade.js
@@ -0,0 +1,11 @@
+function verificarMaioridade(idade){
+    return(idade);
+}
+
+const idade = 19;
+
+if (idade < 18){
+    console.log('Menor de idade');
+}else{
+    console.log('Maior de idade');
+}
